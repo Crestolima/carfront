fe96f1dc5d5a58bbb5f361f31ec6b6e475cac8a8
Cresto Lima <96989398+Crestolima@users.noreply.github.com>
2025-03-22 12:09:22 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://carback-0v6s.onrender.com/';
+const API_URL = 'https://carback-0v6s.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
